5a108a6653471e5b2660548ecc52a17dcce3874d
Matthias Merkl <matthias-merkl@gmx.de>
2024-05-01 20:34:48 +0200

    updated cleanup. Modded games should usually be fine and work as intended after running these scripts

--- 2_fallout_4_install_pre_next_gen.cmd
+++ 2_fallout_4_install_pre_next_gen.cmd
@@ -6,10 +6,37 @@ if exist "steamcmd/steamapps/content/app_377160" (
 		pushd .
 
 		cd /d "%~1"
-		rmdir /s /q Data
+		REM rmdir /s /q Data
+
+		echo Deleting Creation Club content
+		if exist .\Data\ccBGSFO4046-TesCan* (
+			del /f .\Data\ccBGSFO4046-TesCan*
+		)
+		if exist .\Data\ccBGSFO4096-AS_Enclave* (
+			del /f .\Data\ccBGSFO4096-AS_Enclave*
+		)
+		if exist .\Data\ccBGSFO4110-WS_Enclave* (
+			del /f .\Data\ccBGSFO4110-WS_Enclave*
+		)
+		if exist .\Data\ccBGSFO4115-X02* (
+			del /f .\Data\ccBGSFO4115-X02*
+		)
+		if exist .\Data\ccBGSFO4116-HeavyFlamer* (
+			del /f .\Data\ccBGSFO4116-HeavyFlamer*
+		)
+		if exist .\Data\ccFSVFO4007-Halloween* (
+			del /f .\Data\ccFSVFO4007-Halloween*
+		)
+		if exist .\Data\ccOTMFO4001-Remnants* (
+			del /f .\Data\ccOTMFO4001-Remnants*
+		)
+		if exist .\Data\ccSBJFO4003-Grenade* (
+			del /f .\Data\ccSBJFO4003-Grenade*
+		)
+		
 		popd
 		pushd .
-		
+
 		cd steamcmd/steamapps/content/app_377160
 		FOR /D /r %%G in ("depot_*") DO (
 			cd %%~nxG
@@ -34,6 +61,6 @@ if exist "steamcmd/steamapps/content/app_377160" (
 		echo Fallout 4 directory not given !
 	)
 ) else (
-	echo "Fallout 4 files (pre next gen) not yet downloaded"
-	echo "Please run \"1_fallout_4_download_pre_next_gen.cmd\" first !"
+	echo Could not find Fallout 4 files ^(pre next gen^)
+	echo Please run '1_fallout_4_download_pre_next_gen.cmd' first !
 )
