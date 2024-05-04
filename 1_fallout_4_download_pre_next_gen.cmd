330dad812aa958b296652f9d421ce5760f411255
Matthias Merkl <matthias-merkl@gmx.de>
2024-05-04 11:27:03 +0200

    V1.3: no commandline options required, just double-click on the scripts, as described in the readme

--- 1_fallout_4_download_pre_next_gen.cmd
+++ 1_fallout_4_download_pre_next_gen.cmd
@@ -1,20 +1,32 @@
 @echo off
 
+cd /d %~dp0
+
+set /p user=Enter Steam username: 
+set /p pw=Enter Steam password: 
+set /p hd=Download HD textures (y/n): 
+
 pushd .
 
-mkdir steamcmd
-curl https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip --output steamcmd.zip
-tar -xf steamcmd.zip -C steamcmd
+if not exist "steamcmd\steamcmd.exe" ( 
+	mkdir steamcmd
+	curl https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip --output steamcmd.zip
+	tar -xf steamcmd.zip -C steamcmd
+)
+
 cd steamcmd
 
-set /p pw=Enter Steam password:
+if exist "steamapps\content\app_377160" (
+	rmdir /s /q steamapps\content\app_377160
+)
 
-IF "%~2"=="hd" (
-	steamcmd.exe +login %1 %pw% +runscript ../download_depots_hd.txt
+IF "%hd%"=="y" (
+	steamcmd.exe +login %user% %pw% +runscript ../download_depots_hd.txt
 ) ELSE (
-	steamcmd.exe +login %1 %pw% +runscript ../download_depots_no_hd.txt
+	steamcmd.exe +login %user% %pw% +runscript ../download_depots_no_hd.txt
 )
 
 popd
 
 echo Finished !
+pause
--- 2_fallout_4_install_pre_next_gen.cmd
+++ 2_fallout_4_install_pre_next_gen.cmd
@@ -1,11 +1,24 @@
 @echo off
 
+cd /d %~dp0
+
 if exist "steamcmd/steamapps/content/app_377160" (
-	if exist "%~1" (
-		echo copying files to Fallout 4 directory "%~1"
+	setlocal EnableExtensions EnableDelayedExpansion
+
+	:prompt
+	set "fallout_dir=""
+	set /p "fallout_dir=Path to your Fallout 4 installation: "
+	set "fallout_dir=!fallout_dir:"=!"
+	if "!fallout_dir!" == "" goto prompt
+	set "fallout_dir=!fallout_dir:/=\!"
+	if "!fallout_dir:~-1!" == "\" set "fallout_dir=!fallout_dir:~0,-1!"
+	if "!fallout_dir!" == "" goto prompt
+
+	if exist "!fallout_dir!" (
+		echo copying files to "!fallout_dir!"
 		pushd .
 
-		cd /d "%~1"
+		cd /d "!fallout_dir!"
 		REM rmdir /s /q Data
 
 		echo Deleting Creation Club content
@@ -41,11 +54,11 @@ if exist "steamcmd/steamapps/content/app_377160" (
 		FOR /D /r %%G in ("depot_*") DO (
 			cd %%~nxG
 			REM robocopy .\ "%~1" /e /copyall /mt:32
-			xcopy .\ "%~1" /s /e /y /f /x
+			xcopy .\ "!fallout_dir!" /s /e /y /f /x
 			cd ..
 		)
 
-		cd /d "%~1"
+		cd /d "!fallout_dir!"
 		cd ..\..\
 		if exist appmanifest_377160.acf (
 			attrib +r appmanifest_377160.acf
@@ -58,9 +71,12 @@ if exist "steamcmd/steamapps/content/app_377160" (
 
 		echo Finished !
 	) else (
-		echo Fallout 4 directory not given !
+		echo Fallout 4 directory "%fallout_dir%" not found !
+		goto prompt
 	)
 ) else (
 	echo Could not find Fallout 4 files ^(pre next gen^)
 	echo Please run '1_fallout_4_download_pre_next_gen.cmd' first !
 )
+
+pause
